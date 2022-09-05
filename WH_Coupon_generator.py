82f0d924945cb4358770f200dc0c7036159964db
Federico Tensi <federicotensi@gmail.com>
2022-09-05 14:53:01 +0200

    Miglioramento prompt di uscita app

--- WH_Coupon_generator.py
+++ WH_Coupon_generator.py
@@ -334,10 +334,18 @@ def main():
     r = requests.delete("https://api.mail.tm/accounts/"+account_id,
                         headers={"Authorization": "Bearer "+auth_token})
 
+    # Uscita dal programma con stile
+    for item in 5, 4, 3, 2, 1:
+        print(colored("[GOODBYE]", "magenta", attrs=["underline", "bold"]) + " Programma terminato in " +
+              colored("  " + str(item) + "  ", "magenta", "on_white", attrs=["bold"],) + " secondi" + colored(" [GOODBYE]", "magenta", attrs=["underline", "bold"]) + "\r", end="")
+        time.sleep(1)
 
-    print("\n\n\n\n\n\n....\n....\n...\n..\n.\nBye Bye >:D\n\n\n")
+    os.system("cls")
 
-    input("Premi un tasto per chiudere il programma")
+    print(colored("[EXIT] Bye Bye >:D\n\n\r", "magenta", attrs=["bold"]))
+
+    input(colored("[INPUT]", "yellow", attrs=["bold"]) + " Premi " + colored("[ ENTER ]", "white",
+                                                                             "on_green", attrs=["bold"]) + " per chiudere il programma...")
 
     browser.quit()
 
